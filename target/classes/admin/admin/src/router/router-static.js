46005b70bc60535ae28dd8cdd05831b4707c8003
notnames <110458005+notnames@users.noreply.github.com>
2023-03-30 23:48:16 +0800

    Add files via upload

---
+++ target/classes/admin/admin/src/router/router-static.js
@@ -0,0 +1,139 @@
+import Vue from 'vue';
+//配置路由
+import VueRouter from 'vue-router'
+Vue.use(VueRouter);
+//1.创建组件
+import Index from '@/views/index'
+import Home from '@/views/home'
+import Board from '@/views/board'
+import Login from '@/views/login'
+import NotFound from '@/views/404'
+import UpdatePassword from '@/views/update-password'
+import pay from '@/views/pay'
+import register from '@/views/register'
+import center from '@/views/center'
+    import news from '@/views/modules/news/list'
+    import aboutus from '@/views/modules/aboutus/list'
+    import gonglvefenlei from '@/views/modules/gonglvefenlei/list'
+    import systemintro from '@/views/modules/systemintro/list'
+    import yonghu from '@/views/modules/yonghu/list'
+    import jiudianxinxi from '@/views/modules/jiudianxinxi/list'
+    import gonglvelan from '@/views/modules/gonglvelan/list'
+    import config from '@/views/modules/config/list'
+    import chengshi from '@/views/modules/chengshi/list'
+
+
+//2.配置路由   注意：名字
+const routes = [{
+    path: '/index',
+    name: '系统首页',
+    component: Index,
+    children: [{
+      // 这里不设置值，是把main作为默认页面
+      path: '/',
+      name: '系统首页',
+      component: Home,
+      meta: {icon:'', title:'center'}
+    }, {
+      path: '/updatePassword',
+      name: '修改密码',
+      component: UpdatePassword,
+      meta: {icon:'', title:'updatePassword'}
+    }, {
+      path: '/pay',
+      name: '支付',
+      component: pay,
+      meta: {icon:'', title:'pay'}
+    }, {
+      path: '/center',
+      name: '个人信息',
+      component: center,
+      meta: {icon:'', title:'center'}
+    }
+      ,{
+	path: '/news',
+        name: '酒店资讯',
+        component: news
+      }
+      ,{
+	path: '/aboutus',
+        name: '关于我们',
+        component: aboutus
+      }
+      ,{
+	path: '/gonglvefenlei',
+        name: '攻略分类',
+        component: gonglvefenlei
+      }
+      ,{
+	path: '/systemintro',
+        name: '系统简介',
+        component: systemintro
+      }
+      ,{
+	path: '/yonghu',
+        name: '用户',
+        component: yonghu
+      }
+      ,{
+	path: '/jiudianxinxi',
+        name: '酒店信息',
+        component: jiudianxinxi
+      }
+      ,{
+	path: '/gonglvelan',
+        name: '攻略栏',
+        component: gonglvelan
+      }
+      ,{
+	path: '/config',
+        name: '轮播图管理',
+        component: config
+      }
+      ,{
+	path: '/chengshi',
+        name: '城市',
+        component: chengshi
+      }
+    ]
+  },
+  {
+    path: '/login',
+    name: 'login',
+    component: Login,
+    meta: {icon:'', title:'login'}
+  },
+  {
+    path: '/board',
+    name: 'board',
+    component: Board,
+    meta: {icon:'', title:'board'}
+  },
+  {
+    path: '/register',
+    name: 'register',
+    component: register,
+    meta: {icon:'', title:'register'}
+  },
+  {
+    path: '/',
+    name: '系统首页',
+    redirect: '/index'
+  }, /*默认跳转路由*/
+  {
+    path: '*',
+    component: NotFound
+  }
+]
+//3.实例化VueRouter  注意：名字
+const router = new VueRouter({
+  mode: 'hash',
+  /*hash模式改为history*/
+  routes // （缩写）相当于 routes: routes
+})
+const originalPush = VueRouter.prototype.push
+//修改原型对象中的push方法
+VueRouter.prototype.push = function push(location) {
+   return originalPush.call(this, location).catch(err => err)
+}
+export default router;
