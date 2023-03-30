830b10146eadf2dd0c768dd6455f5e5efaed556c
notnames <110458005+notnames@users.noreply.github.com>
2023-03-30 23:41:06 +0800

    Add files via upload

---
+++ target/classes/front/front/src/config/config.js
@@ -0,0 +1,21 @@
+export default {
+    baseUrl: 'http://localhost:8080/springbootu1xmu/',
+    indexNav: [
+        {
+            name: '首页',
+            url: '/index/home'
+        },
+        {
+            name: '酒店信息',
+            url: '/index/jiudianxinxi'
+        },
+        {
+            name: '攻略栏',
+            url: '/index/gonglvelan'
+        },
+        {
+            name: '酒店资讯',
+            url: '/index/news'
+        },
+    ]
+}
